31e0f5661943599ab8bda4a01e1c63e363ce9b56
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-02-28 17:05:25 -0600

    se hicieron los dbvalidators y el generarJwt

---
+++ src/helpers/db-validators.js
@@ -0,0 +1,17 @@
+import User from '../users/user.model.js';
+
+export const existenteEmail = async (correo = '') =>{
+    const existeEmail = await User.findOne({ correo });
+
+    if (existeEmail) {
+        throw new Error(`El email ${correo} ya fue registrado`);
+    }
+}
+
+export const existeUsuarioById = async (id = '') => {
+    const existeUsuario = await User.findOne(id);
+
+    if (!existeUsuario) {
+        throw new Error(`El ID: ${id} No existe`);
+    }
+}
